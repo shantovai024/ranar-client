fdb30d120de3a49a61f6db8e5d947019059c888d
Istiak Ahmed <istiakahmed024@gmail.com>
2022-11-10 12:15:05 +0600

    initial header done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import './Header.css'
+import logo from '../images/logo.png'
 import { Link } from 'react-router-dom';
 
 
 const Header = () => {
     return (
-        <div>
-            <div className="logo-area">
-                <Link to='/'><img src="" alt="" /></Link>
+        <div className='row mb-20'>
+            <div className="col-xl-3">
+                <div className="logo-area">
+                    <Link to='/'><img src={logo} alt="" /></Link>
+                </div>
             </div>
-            <nav>
-                <Link to='/about'>About</Link>
-                <Link to='/blog'>Blog</Link>
-            </nav>
+            <div className="col-xl-9 d-flex justify-content-end align-items-center">
+                <nav className='menu-area'>
+                    <Link to='/about'>About</Link>
+                    <Link to='/blog'>Blog</Link>
+                </nav>
+            </div>
         </div>
     );
 };
